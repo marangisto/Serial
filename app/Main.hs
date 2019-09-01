3a172ecb1e0b5a26068c346aedf1578a91da2243
Bengt Marten Agren <maraingist@me.com>
2019-09-01 23:45:45 +0800

    Working serial read.

--- app/Main.hs
+++ app/Main.hs
@@ -2,28 +2,30 @@
 module Main where
 
 import System.Console.CmdArgs
+import qualified Data.ByteString.Char8 as B
+import System.Hardware.Serialport
+import System.Console.Haskeline
+import Control.Concurrent
+import Control.Monad.IO.Class
 import Control.Monad
 import Data.List (find)
 import Data.Maybe
 import System.IO
 
 data Options = Options
-    { schema_version    :: Bool
-    , interrupt         :: Bool
-    , files             :: [FilePath]
+    { port      :: FilePath
+    , files     :: [FilePath]
     } deriving (Show, Eq, Data, Typeable)
 
 options :: Main.Options
 options = Main.Options
-    { schema_version = def &= help "Show schema version"
-    , interrupt = def &= help "Generate interrupt vector table"
+    { port = def &= help "serial port"
     , files = def &= args &= typ "FILES"
     } &=
     verbosity &=
-    help "Generate device descriptions from SVD files" &=
-    summary "SVD2CPP v0.0.0, (c) Bengt Marten Agren 2018-2019" &=
-    details [ "SVD2CPP generated device header files for ARM-based"
-            , "MCUs based on vendor SVD files (see CMSIS)."
+    help "Serial terminal" &=
+    summary "Serial v0.0.0, (c) Bengt Marten Agren 2019" &=
+    details [ "This is a simple serial terminal"
             ]
 
 main :: IO ()
@@ -31,4 +33,8 @@ main = do
     opts@Options{..} <- cmdArgs options
     hSetNewlineMode stdout noNewlineTranslation
     print opts
+    h <- hOpenSerial port defaultSerialSettings { commSpeed = CS115200 }
+    forever $ do
+        hGetLine h >>= putStrLn
+        hFlush stdout
 
